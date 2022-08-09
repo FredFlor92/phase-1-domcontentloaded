a5013b8ef4ec5b92d768e5d277dcbf011b824d21
Freddy Florial <freddyflorial@gmail.com>
2022-08-08 20:14:20 -0400

    commit tree 0d8a37a58b16fa9e6d8bfbda9b7f0e9eef2ebd4c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+updateParagraphText();
+});
+
+function updateParagraphText(){
+    const textToUpdate = document.querySelector("#text").textContent = "This is really cool!";
+
+    return textToUpdate;
+}
